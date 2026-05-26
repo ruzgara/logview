21f75b72c69440ab68e38def5f676380edf96be9
Ruzgar Altiner <ruzgar.altiner@gmail.com>
2026-05-27 00:26:52 +0100

    added new user auth endpoint hooks to the backend. Also merged the migrations, as well as creating a default settings migration

---
+++ backend/pb_migrations/0_initial_settings.js
@@ -0,0 +1,31 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+const TempEmail = "_@l.l"; // must be same as in pb_hooks/users.pb.js
+migrate((app) => {
+  let userEmail = $os.getenv("USER_EMAIL");
+  const userPass = $os.getenv("USER_PASS");
+  const detailsProvided = userEmail && userPass;
+
+  if (!detailsProvided) userEmail = TempEmail;
+
+  let superusers = app.findCollectionByNameOrId("_superusers");
+  let record = new Record(superusers);
+
+  record.setEmail(userEmail);
+
+  if (detailsProvided)
+    record.setPassword(userPass);
+  else
+    record.setRandomPassword();
+
+  app.save(record);
+
+  if (detailsProvided) {
+    let usersCollection = app.findCollectionByNameOrId("users");
+    let userRecord = new Record(usersCollection);
+    userRecord.setEmail(userEmail);
+    userRecord.setPassword(userPass);
+    userRecord.setVerified(true);
+    app.save(userRecord);
+  }
+});
